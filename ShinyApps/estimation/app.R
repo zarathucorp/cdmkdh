02a3af8384ea25f0f9af7719666719b04ed00ba1
Jinseob Kim <jinseob2kim@gmail.com>
2019-07-21 13:18:41 +0900

    s

--- ShinyApps/estimation/app.R
+++ ShinyApps/estimation/app.R
@@ -1,4 +1,4 @@
-library(shiny);library(DT);library(shinycustomloader);library(shinyhttr);library(shinyWidgets)
+library(shiny);library(DT);library(shinycustomloader);library(shinyhttr);library(shinyWidgets);library(data.table)
 options(shiny.sanitize.errors = F)
 options(shiny.maxRequestSize=30*1024^2)
 
@@ -106,9 +106,9 @@ server <- function(input, output, session) {
     
     output$typeOutcome <-renderUI({
       if (length(data()$fig.km) == 0){
-        radioButtons("type_outcome", "Result select", choices = c("attrition", "psModelCoef", "outcomeModelTreatmentEstimate", "populationCounts", "outcomeCounts", "timeAtRisk", "Others"), selected = "attrition", inline = T)
+        radioButtons("type_outcome", "Result select", choices = c("Hazard ratio",  "psModelCoef", "attrition", "Event", "Others"), selected = "Hazard ratio", inline = T)
       } else{
-        radioButtons("type_outcome", "Result select", choices = c("attrition", "psModelCoef", "outcomeModelTreatmentEstimate", "populationCounts", "outcomeCounts", "timeAtRisk", "kaplanMeier", "Others"), selected = "attrition", inline = T)
+        radioButtons("type_outcome", "Result select", choices = c("kaplanMeier", "Hazard ratio", "psModelCoef",  "attrition", "Event", "Others"), selected = "kaplanMeier", inline = T)
       }
         
     })
@@ -230,13 +230,26 @@ server <- function(input, output, session) {
         if (input$type_outcome == "psModelCoef"){
           #out.df <- data.frame(Coefficients = res[[input$type_outcome]])
           
-          datatable(data.frame(Covariates = names(res[[input$type_outcome]]), coefficients = res[[input$type_outcome]]),  rownames = F, filter = "top", extensions= "Buttons", caption = "PS model coefficients", 
+          datatable(data.frame(Covariates = names(res[[input$type_outcome]]), HR = exp(res[[input$type_outcome]])),  rownames = F, filter = "top", extensions= "Buttons", caption = "PS model coefficients", 
                     options = c(jstable::opt.data(input$type_outcome), list(scrollX = TRUE))) %>% 
             formatRound(2, digits = 3)
+        } else if (input$type_outcome == "Hazard ratio"){
+          hr <- res$outcomeModelTreatmentEstimate
+          pv <- 2 * (1 - pnorm(abs(hr$logRr/hr$seLogRr)))
+          pv <- ifelse(pv < 0.001, "< 0.001", round(pv, 3))
+          tb.hr <- data.table(`HR (95% CI)` = paste0(round(exp(hr$logRr), 3), " (", round(exp(hr$logLb95), 3), "-", round(exp(hr$logUb95), 3), ")"), `P value` = pv)
+          datatable(tb.hr,  rownames = F, extensions= "Buttons", caption = "Hazard ratio", 
+                    options = c(jstable::opt.tbreg("Hazard ratio"), list(scrollX = TRUE))) 
         } else if (input$type_outcome == "Others"){
           name.tb <- setdiff(names(res), c("targetId", "comparatorId", "attrition", "psModelCoef", "outcomeModelTreatmentEstimate", "populationCounts", "outcomeCounts", "timeAtRisk"))
           datatable(t(sapply(name.tb, function(x){res[[x]]})), rownames = F,  extensions= "Buttons", caption = input$type_outcome, 
                     options = c(jstable::opt.data(input$type_outcome), list(scrollX = TRUE))) 
+        } else if (input$type_outcome == "Event"){
+          tb.oth <- t(data.frame(as.integer(res$populationCounts[, 2:3]), as.integer(res$timeAtRisk), as.integer(res$outcomeCounts[5:6])))
+          colnames(tb.oth) <- c("Treated", "Comparator")
+          rownames(tb.oth) <- c("Population", "Time at Risk", "Event")
+          datatable(tb.oth,  rownames = T, extensions= "Buttons", caption = input$type_outcome, 
+                    options = c(jstable::opt.data("Event"), list(scrollX = TRUE))) 
         } else {
           datatable(res[[input$type_outcome]],  rownames = F, extensions= "Buttons", caption = input$type_outcome, 
                     options = c(jstable::opt.data(input$type_outcome), list(scrollX = TRUE))) 
